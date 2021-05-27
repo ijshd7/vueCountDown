6a6fcdb02b8a65d5cf28d72dcd100e6534966f7c
Isaiah Sherrill <Isaiah.Sherrill@panerabread.com>
2021-05-27 17:25:06 -0500

    prototype

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Counter
+      :year="2021"
+      :month="4"
+      :date="27"
+      :hour="16"
+      :minute="37"
+      :second="44"
+      :millisecond="31"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Counter from '@/components/Counter.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Counter
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
